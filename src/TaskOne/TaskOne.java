1bc7e3cf66e220386f4ff6b41c1a8c5709d9bf19
Maximfsda <lisitsin.maxi@yandex.ru>
2022-10-02 00:19:56 +1000

    HW1

--- src/TaskOne/TaskOne.java
+++ src/TaskOne/TaskOne.java
@@ -3,7 +3,7 @@ package TaskOne;
 import java.util.Scanner;
 
 public class TaskOne {
-    public static int CheckWhetherTheYearLeap(int years) {
+    public static int checkWhetherTheYearLeap(int years) {
         int year = years;
         if ((year % 4 == 0) && year % 100 != 0) {
             System.out.println(year + " — високосный год.");
@@ -18,6 +18,6 @@ public class TaskOne {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите год : ");
         int year = in.nextInt();
-        CheckWhetherTheYearLeap(year);
+        checkWhetherTheYearLeap(year);
     }
 }
